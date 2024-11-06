b53c7a98889092e2dea6b5c14a3a5ce60315d832
Leo <leolair.pro@gmail.com>
2024-11-06 16:59:59 +0100

    start create group

--- src/NewGroup/NewGroup.jsx
+++ src/NewGroup/NewGroup.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React ,{useEffect, useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import { useNav } from "../Context/NavContext";
+import back from '../img/left-arrow.png'
+import './style.css'
+import { useUser } from '../Context/UserContext';
+
 
 const NewGroup = () => {
-    return(
-        <h1>
-            new group
-        </h1>
+    const navigate = useNavigate(); 
+    const {CurrentPage, setCurrentPage} = useNav('');
+    const {user, updateUser } = useUser();
+    const [friends, setFriends] = useState('')
+
+
+    const backNavigate = () =>{
+        navigate('/groups');
+        setCurrentPage('/groups');
+    }
+
+    useEffect(() => {    
+        setFriends(user.friends);
+        console.log(friends);
+        
+
+    }, []); 
+
+
+        return(
+        <div>
+            <a className="see_groups"  onClick={backNavigate}>
+                <h4>Voir les groupes</h4>
+                <img id="see_group" src={back}/>
+            </a>
+
+            <form>
+                <input type="text" placeholder="Nom du groupe"/>
+               
+
+            </form>
+        </div>
     )
 }
 
